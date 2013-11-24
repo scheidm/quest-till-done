cb434108cc5fe2ba5a81cdf57553e8987dac93de
scheidm <scheidm@wellcomposed.net>
2013-11-24 10:06:23 -0500

    finished the definition of action, as refined from original task and project definition, in the dev document

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,11 @@
 class Action < ActiveRecord::Base
   has_many :nodes, through: :pomodoros
-
+  has_many :actions
+  def is_task?
+    self.actions.nil?
+  end
+  
+  def is_top_lvl_project?
+    self.parent_id.nil?
+  end
 end
