815c1906581fefe1b391d5e1deae01a2674b5157
scheidm <scheidm@wellcomposed.net>
2013-11-24 11:19:02 -0500

    fixup of action model validations and methods

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,8 +1,11 @@
 class Action < ActiveRecord::Base
+  STATES = ["Unstarted", "In Progress", "Completed"]
+  validates_inclusion_of :status, :in => STATES
+  validates_presence_of :name
   has_many :nodes, through: :pomodoros
   has_many :actions
   def is_task?
-    self.actions.nil?
+    !self.actions.empty?
   end
   
   def is_top_lvl_project?
