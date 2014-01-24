816b23db7542564056b85308e86457cd682f58b2
Kevin Huang <hkshldubuntu@gmail.com>
2014-01-24 15:22:44 -0500

    added devise, admin, user model, default ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Quest::Application.routes.draw do
+  devise_for :admins
+  devise_for :users
   resources :pomodoros do
     collection do
       get :getTree
@@ -9,7 +11,7 @@ Quest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+root = User.create :email => "root@qtd.com", :password => "123"
