dbb92719e476b6057461d680669fa5bc644a5454
scheidm <scheidm@wellcomposed.net>
2014-01-25 11:47:32 -0500

    finished records change

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -78,8 +78,8 @@ class PomodorosController < ApplicationController
     #  _endTime = (item.end_time.nil? ) ? 'Now' : item.end_time.to_formatted_s(:long)
     #  @TreeData = ({:data => item.created_at.to_formatted_s(:long) + ' - ' + _endTime, :attr => { :href => '/pomodoros/' + item.id.to_s, :rel => 'pomodoro' }})
     #  @TreeData[:children] = children = []
-    #  item.records.each {|node|
-    #    type = node.specific
+    #  item.records.each {|record|
+    #    type = record.specific
     #    children <<  ({:data => type.class.name, :attr => { :href => '/'+ type.class.name.downcase + 's/' + type.id.to_s, :rel => type.class.name }})
     #  }
     #  data.push(@TreeData)
