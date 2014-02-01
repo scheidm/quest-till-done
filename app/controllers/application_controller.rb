ab23dbe23f16a79cb60d0f6dd3ae0fa7d030b4b4
Khong <khong@Khong.(none)>
2014-02-01 18:50:37 -0500

    Linked username to current logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter  :configure_permitted_parameters, if: :devise_controller?
   before_filter :load_user
+  before_filter :authenticate_user!
 
   protected
 
