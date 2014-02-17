34e667686d882a22d0c0cfed1395f789404ee4e4
Mark Scheid <scheidm@wellcomposed.net>
2014-02-17 16:27:23 -0500

    yard currently showing ALL methods including private and protected, overidding default behavior. documentation for quest search

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,7 +1,15 @@
+##
+#===A note on searching
+#While handled by solr, below is the details of how to operate Quest.search
+#Using solr, define full text search for quests and their records
+#@param full_text [String] the text to be found, 'Ruby on Rails', #'documentation'
+#@param :estimated_cost
 class Quest < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :skills
   has_many :records
+  has_many :links
+  has_many :notes
   belongs_to :campaign, :class_name => 'Quest'
   has_many :all_quests, :class_name => 'Quest', :foreign_key => 'campaign_id'
   belongs_to :parent, :class_name => 'Quest'
@@ -9,7 +17,29 @@ class Quest < ActiveRecord::Base
   belongs_to :user
   after_create :set_campaign
 
+  searchable do
+    text :name, :description
+    text :status
+    integer :estimated_cost, :current_cost, :parent__id, :campaign_id
+    time :updated_at,:created_at
+    text :records do
+      records.map{ |r| r.description }
+    end
+    text :records do
+      records.map{ |r| r.description }
+    end
+    text :links do
+      links.map{ |l| l.url }
+    end
+    text :notes do
+      notes.map{ |n| n.description }
+    end
+  end
+
   protected
+  ##
+  # = this is some documentation
+  #
   def set_campaign
      if(self.campaign_id.nil?)
        self.reload
