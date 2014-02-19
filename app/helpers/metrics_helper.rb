d927efe8c8484729fa462278f10b083119a2eaa2
Mark Scheid <scheidm@wellcomposed.net>
2014-02-18 20:23:38 -0500

    more reorganization of misplaced code

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -1,18 +1,36 @@
 # Get list of completed tasks
 # @return [Array] Completed tasks
-def getCompletedTasks user
+def get_completed_tasks user
 
 end
 
 
 # Get list of completed projects
 # @return [Array] Completed projects
-def getCompletedProjects user
+def get_completed_projects user
 
 end
 
 # Get efficiency based on time and completed tasks/projects
 # @return [Float] Efficiency Number
-def getEfficiency user
+def get_efficiency user
+
+# Recent Levels for a period of time
+# @return [Array] Array of levels against time
+def level_over_time
+
+end
+
+# Skill Time Investment VS Completed Tasks
+# @return [Hash] Hash of Skill Investment vs Completed tasks
+def skill_time_vs_completed_tasks
+
+end
+
+# Performance Stats
+# @return [JSON] Performance stats for all time levels, quest completed, compaing completed and time estimacy accuracy
+def performance_overview
+
+end
 
 end
