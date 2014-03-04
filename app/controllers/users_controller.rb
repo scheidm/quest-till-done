7d91aa64e263829f97284540ca83d28c9513543e
Kevin Huang <kevin@khuang.org>
2014-03-04 18:07:15 -0500

    added user github account match table, updated schema and controller support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,23 @@ class UsersController < ApplicationController
     list_projects
     list_issues 'scheidm', 'quest-till-done'
     commits 'scheidm', 'quest-till-done'
-    import 'scheidm', 'quest-till-done'
+    initial_import 'scheidm', 'quest-till-done'
   end
 
 
+  def github_list
+    login
+    list_projects
+    @projects = Githubaccounts.where(user_id: current_user)
+  end
+
+  def github_project_import
+    initial_import params[:github_user], params[:repo_name]
+  end
+
+  def github_project_del
+
+  end
 
 
 end
