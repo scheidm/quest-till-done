e03a890fd91e16bc2d8889321ac702121cffe9f4
Kevin Huang <kevin@khuang.org>
2014-03-05 19:21:23 -0500

    change issue to quest

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,8 +1,8 @@
-class Issue < Record
+class Issue < Quest
   # Validate presence of description and url
   validates_presence_of :description, :url, :allow_blank => false
 
   def self.model_name
-    Record.model_name
+    Quest.model_name
   end
 end
