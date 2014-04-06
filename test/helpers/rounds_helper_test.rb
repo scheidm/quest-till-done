a475929b166a05a2139e52f3308ad8a1edc804fa
Kevin Huang <kevin@khuang.org>
2014-04-05 20:15:25 -0400

    more test on rounds and cleanup

--- test/helpers/rounds_helper_test.rb
+++ test/helpers/rounds_helper_test.rb
@@ -2,18 +2,15 @@ require 'test/unit'
 
 class RoundHelperTest < ActionView::TestCase
   include Devise::TestHelpers
-  include TimerHelper
-
 
   # Called before every test method runs. Can be used
   # to set up fixture information.
   def setup
     @campaign = Quest.find(1)
-
   end
 
   test 'Can create a new Round' do
-    assert !create_round(Record.find(1), "create", @campaign)
+    assert !create_round(Record.find(1), 'create', @campaign), 'Cannot create round properly'
   end
 
 
--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -5,8 +5,8 @@ class RoundTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @quest_round = Round.find(1)
-    @campaign_round = Round.find(1)
+    @campaign_round = Campaign.find(1)
+    @round = Round.find(1)
   end
 
   def teardown
@@ -15,14 +15,22 @@ class RoundTest < ActiveSupport::TestCase
 
 
 
-  test "Round must have campaign" do
+  test 'Round must have campaign' do
     @empty_campaign = Campaign.new
-    assert !Round.create_event(Record.find(1), "test", @empty_campaign), 'Round without campaign is saved'
+    assert !Round.create_event(Record.find(1), 'test', @empty_campaign), 'Round without campaign is saved'
   end
 
 
   test 'Round must have operation' do
     assert !Round.create_event(Record.find(1), "", @campaign_round), 'Round without operation is saved'
+  end
+
+  test 'Round can find model' do
+    assert !!@round.related_obj
+  end
+
+  test 'Round can find link' do
+    assert !!@round.related_link
   end
 
 
