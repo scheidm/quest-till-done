b22d67521d2d7d901ea31d4b0b477c5760d7c68d
Kevin Huang <kevin@khuang.org>
2014-04-24 19:25:31 -0400

    bind githubrepo to groups instead of users

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_many :campaigns
   has_many :rounds
+  has_many :github_repos
   has_and_belongs_to_many :users
   has_and_belongs_to_many :admins, class_name: "User", join_table: "admins_groups"
   # Leave the group while preventing orphan groups. If the user is the last
