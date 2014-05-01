b6e2acd1eb7a65e756be8e3c6ff9d36805e4e94a
Kevin Huang <kevin@khuang.org>
2014-04-30 22:13:42 -0400

    temporary fix for messaging / notification system

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430190007) do
+ActiveRecord::Schema.define(version: 20140501015064) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20140430190007) do
   create_table "admins_groups", id: false, force: true do |t|
     t.integer "group_id"
     t.integer "user_id"
+  end
+
+  create_table "conversations", force: true do |t|
+    t.string   "subject",    default: ""
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "delayed_jobs", force: true do |t|
@@ -90,18 +96,24 @@ ActiveRecord::Schema.define(version: 20140430190007) do
   end
 
   create_table "notifications", force: true do |t|
-    t.string   "message_type"
-    t.boolean  "dismissed"
-    t.integer  "source_id"
-    t.string   "source_type"
-    t.integer  "target_id"
-    t.string   "target_type"
-    t.string   "body"
-    t.string   "action_type"
-    t.integer  "authorization_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "type"
+    t.text     "body"
+    t.string   "subject",              default: ""
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.integer  "conversation_id"
+    t.boolean  "draft",                default: false
+    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                           null: false
+    t.integer  "notified_object_id"
+    t.string   "notified_object_type"
+    t.string   "notification_code"
+    t.string   "attachment"
+    t.boolean  "global",               default: false
+    t.datetime "expires"
   end
+
+  add_index "notifications", ["conversation_id"], name: "index_notifications_on_conversation_id", using: :btree
 
   create_table "quests", force: true do |t|
     t.string   "name",                           null: false
@@ -125,6 +137,20 @@ ActiveRecord::Schema.define(version: 20140430190007) do
   end
 
   add_index "quests", ["slug"], name: "index_quests_on_slug", using: :btree
+
+  create_table "receipts", force: true do |t|
+    t.integer  "receiver_id"
+    t.string   "receiver_type"
+    t.integer  "notification_id",                            null: false
+    t.boolean  "is_read",                    default: false
+    t.boolean  "trashed",                    default: false
+    t.boolean  "deleted",                    default: false
+    t.string   "mailbox_type",    limit: 25
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
+
+  add_index "receipts", ["notification_id"], name: "index_receipts_on_notification_id", using: :btree
 
   create_table "records", force: true do |t|
     t.string   "type"
@@ -256,5 +282,9 @@ ActiveRecord::Schema.define(version: 20140430190007) do
     t.integer "user_id"
     t.integer "group_id"
   end
+
+  add_foreign_key "notifications", "conversations", name: "notifications_on_conversation_id"
+
+  add_foreign_key "receipts", "notifications", name: "receipts_on_notification_id"
 
 end
