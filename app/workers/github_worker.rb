dae89b2a549fcf85d5d79553d29257a35b5fd650
Kevin Huang <kevin@khuang.org>
2014-05-09 04:04:33 -0400

    working github background work.

--- app/workers/github_worker.rb
+++ app/workers/github_worker.rb
@@ -1,10 +1,11 @@
 class GithubWorker
   include Sidekiq::Worker
   include GithubHelper
-  sidekiq_options queue: "high"
-  sidekiq_options retry: false
+  # sidekiq_options retry: false
 
   def perform(user)
+    current_user = User.find(user)
+    login
     github_update_all_projects(user)
   end
 
