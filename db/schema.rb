c14b7a1c7cda9113f0b5127d27d96eb7ba2df02e
Kevin Huang <kevin@khuang.org>
2014-05-12 22:50:01 -0400

    github worked as old ways

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512235749) do
+ActiveRecord::Schema.define(version: 20140513022648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,6 +169,8 @@ ActiveRecord::Schema.define(version: 20140512235749) do
     t.string   "code_content_type"
     t.integer  "code_file_size"
     t.datetime "code_updated_at"
+    t.string   "github_user"
+    t.string   "project_name"
   end
 
   create_table "rounds", force: true do |t|
