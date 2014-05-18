467436b4220e29437f299f7a6765dfd42b47ac93
Kevin Huang <kevin@khuang.org>
2014-05-18 12:42:16 -0400

    forget to push the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513022648) do
+ActiveRecord::Schema.define(version: 20140518155706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,7 +181,7 @@ ActiveRecord::Schema.define(version: 20140513022648) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "campaign_id"
-    t.string   "group_id"
+    t.integer  "group_id"
   end
 
   create_table "sessions", force: true do |t|
