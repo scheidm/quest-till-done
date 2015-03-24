4b01f1676b9efecab80ca8d565faf4943918dfa1
scheidm <scheidm@wellcomposed.net>
2015-03-24 08:56:59 -0400

    fixed #158 core issue, display all records in one block

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,12 @@
 #location of the file on the server to allow retrieval
 class Image < Record
   # Validate presence of description and url
+  
 
 
+  def url
+    return self.code.url
+  end
   validates_attachment_presence :code
   def destroy_rounds
     Round.where( type: "Image").where(event_id: self.id).destroy_all
