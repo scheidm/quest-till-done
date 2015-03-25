161995e8d79d7051d8485326adc1472739dd2e80
scheidm <scheidm@wellcomposed.net>
2015-03-24 20:09:51 -0400

    refactored for issue #150, deferring to T2

--- app/concerns/json_generator.rb
+++ app/concerns/json_generator.rb
@@ -3,10 +3,9 @@ module JsonGenerator
   module TimelineModule
     include ActionView::Helpers::DateHelper
     def generateTimeline(rounds)
-      format = '%I:%M%p'
       data = []
       rounds.each do |round|
-        data << {:id => round.event_id, :data => "#{trunc(round.event_description, 125)} #{round.type}: #{trunc(round.related_obj.to_s,125)} - #{round.created_at.to_time.strftime(format) }", :attr => { :rel => round.type, :href => round.related_link }}
+        data << {:id => round.event_id, :data => round.to_str, :attr => { :rel => round.type, :href => round.related_link }}
       end
       return data.to_json
     end
